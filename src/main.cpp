f0ccfe5a7663300b61a46ff9b992564e0bcb82a5
Oliver Atkinson <oliveratkinsoneng@gmail.com>
2023-02-24 21:18:26 -0700

    I have a headache now, deciamals kinda work

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 SevSeg sevseg; // Instantiate a seven segment controller object
 const int BUFFER_SIZE = 32;
-void write(char *buf, int len, int start);
+void write(char *buf, int buf_len, int start);
 
 void setup()
 {
@@ -11,11 +11,11 @@ void setup()
     byte digitPins[] = {10, 15, A0, 4};
     byte segmentPins[] = {16, A1, 6, 8, 9, 14, 5, 7};
 
-    bool resistorsOnSegments = false;     // 'false' means resistors are on digit pins
-    byte hardwareConfig = COMMON_CATHODE; // See README.md for options
-    bool updateWithDelays = false;        // Default 'false' is Recommended
-    bool leadingZeros = false;            // Use 'true' if you'd like to keep the leading zeros
-    bool disableDecPoint = false;         // Use 'true' if your decimal point doesn't exist or isn't connected
+    bool resistorsOnSegments = false; // not using any resistors... but this works, so don't change it
+    byte hardwareConfig = COMMON_CATHODE;
+    bool updateWithDelays = false; // This is their recommended value
+    bool leadingZeros = false;
+    bool disableDecPoint = false;
 
     sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments,
                  updateWithDelays, leadingZeros, disableDecPoint);
@@ -33,31 +33,23 @@ void loop()
     if (Serial.available()) // only run serial data is incoming
     {
         read_len = Serial.readBytes(buffer, BUFFER_SIZE);
-
-        // write(buffer, read_len, 0);
     }
-    
+
     int now = millis();
     if (now - start_time > 500)
     {
         start_time = now;
 
-        // FIXME tries to scroll when < 4 charsit
         if (read_len <= 4)
         {
             start = 0;
         }
         else
         {
-            // start = start < read_len ? start + 4 : 0; // display chunks of 4 like a slide show
-            start = start < read_len ? start + 1 : 0; // display scrolling text 
+            start = start < read_len ? start + 1 : 0; // display scrolling text
         }
         write(buffer, read_len, start);
-        Serial.print("Start at");
-        Serial.print(start);
-        Serial.print(" Read ");
-        Serial.println(read_len);
-    }   
+    }
 
     sevseg.refreshDisplay();
 }
@@ -67,27 +59,50 @@ void loop()
 //
 // `buf` is the buffer you are displaying from
 //
-//  `len` is the length of of `buf` that you want to read from.
+// `buf_len` is the length of of `buf` that you want to read from.
 //
 // `start` is the index to start the display at.
-void write(char *buf, int len, int start)
+void write(char *buf, int buf_len, int start)
 {
-    static char display_buffer[4];
-    // TODO needs to ignore "." as a char, the display has a dedicated "."
+    static char display_buffer[8];
 
-    int word_len = len > 4 ? 4 : len;
-    // the amount of chars the word needs to be shifted to the right
-    int shift = 4 - word_len;
-    // fill the unused space with space char
-    for (int i = 0; i < shift; i++)
+    int not_counted = 0;
+    for (int i = start; i < buf_len && i < 8; i++)
     {
-        display_buffer[i] = ' ';
-    }
-    // fill the buffer with the text (right-justified)
-    for (int i = shift; i < 4; i++)
-    {
-        display_buffer[i] = buf[i - shift + start];
+        if (buf[i] == '.')
+        {
+            not_counted++;
+        }
     }
 
+    int shift = 4 - buf_len + not_counted;
+    shift = shift < 0 ? 0 : shift;
+    
+    Serial.print("buf_len: "); 
+    Serial.println(buf_len);
+
+    Serial.print("start: ");
+    Serial.println(start);
+
+    Serial.print("shift: ");
+    Serial.println(shift);
+
+    Serial.print("n-c: ");
+    Serial.println(not_counted);
+
+    // FIXME not respecting start for actuall start location
+    for (int i = start; i < buf_len && i < 8; i++)
+    {
+        display_buffer[i + shift] = buf[i];
+    }
+
+    if (shift) // 0 == false
+    {          // make blank leading space
+        for (int i = start; i < shift; i++)
+        {
+            display_buffer[i] = ' ';
+        }
+    }
+    // FIXME going from 1.2.3.4. -> 1.2.3.4 (no dot) doesn't remove the last dot
     sevseg.setChars(display_buffer);
 }
